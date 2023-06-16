3f6d2d4e0fcc7157a41669c1676a8bccb0c2980d
sehan22 <sehanranaweera2002@gmail.com>
2023-06-16 11:40:00 +0530

    CAL assignment finalize

--- InClass Assignments/JS/CAL/assests/JScript/js1.js
+++ InClass Assignments/JS/CAL/assests/JScript/js1.js
@@ -1,94 +1,27 @@
-/*
-let appendVal;
-let textD;
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll('button');
 
-document.addEventListener('keydown',function (event){
-
-    /!*console.log(event.key);
-    console.log(event.code);*!/
-
-    const conKeys = ["-", "+", "*", "/"];
-
-    if (event.key >= "0" && event.key <= "9"){
-        showKey(event.key);
-    }
-
-    if (conKeys.includes(event.key) ) {
-        if (!/[+\-*\/=]$/.test(appendVal)){
-            showKey(event.key);
+let string = "";
+let array = Array.from(buttons);
+array.forEach(button => {
+    button.addEventListener('click',(e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
         }
-    }
-
-    if (event.key==="Backspace"){
-        removeLastDigit(appendVal);
-    }
-    if (event.key==="Escape"){
-        appendVal="";
-        textD.val(appendVal);
-    }
-    if (event.key==="Enter"){
-        if (!/[+\-*\/=]$/.test(appendVal)){
-            let results=eval(appendVal);
-            console.log(results);
-            textD.val(""+results);
+        
+        else if (e.target.innerHTML == 'AC') {
+            string = '';
+            input.value = string;
         }
-    }
-
-});
-
-$(`#calBody`).click(function (event){
-    let target = event.target.closest("button");
-    const conKeys = ["-", "+", "*", "/"];
-
-    if (target.innerText >= "0" && target.innerText <= "9"){
-        showKey(target.innerText);
-    }
-
-    if (conKeys.includes(target.innerText)){
-        if (!/[+\-*\/=]$/.test(appendVal)){
-            showKey(target.innerText);
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length-1);
+            input.value = string;
         }
-    }
-
-    if (target.innerText==="C"){
-        removeLastDigit(appendVal);
-    }
-
-    if (target.innerText==="AC"){
-        appendVal="";
-        textD.val(appendVal);
-    }
-
-    if (target.innerText==="="){
-        if (!/[+\-*\/=]$/.test(appendVal)){
-            let results=eval(appendVal);
-            console.log(results);
-            textD.val(""+results);
+        
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
         }
-
-    }
-
+    })
 })
-
-
-function showKey(key) {
-    textD = $(`#textD`);
-    let currentVal=textD.val();
-    appendVal=currentVal+""+key;
-
-    textD.val(appendVal);
-}
-
-function removeLastDigit(currentVal){
-
-    let lastChar = currentVal.charAt(currentVal.length - 1);
-    const conKeys = ["-", "+", "*", "/","="];
-
-    if (conKeys.includes(lastChar)){
-        console.log(lastChar);
-    }
-
-    let newVal = currentVal.substring(0, currentVal.length - 1);
-    appendVal=newVal
-    textD.val(appendVal);
-}*/
