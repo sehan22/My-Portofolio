f95f78a03906c5483ab71a02a22bfd52518e38cc
sehan22 <sehanranaweera2002@gmail.com>
2023-06-24 00:11:15 +0530

    Web POS updated

--- InClass Assignments/JS/SPA/cutomerjs.js
+++ InClass Assignments/JS/SPA/cutomerjs.js
@@ -38,6 +38,7 @@ document
     row.appendChild(col3);
     row.appendChild(col4);
 
+    alert("Are you sure you want to save customer information?");
     let customerTable = document.getElementById("customerTable");
     customerTable.appendChild(row);
   });
@@ -62,28 +63,23 @@ document
 document
   .getElementById("btnCustomerUpdate")
   .addEventListener("click", function () {
-    alert("Customer");
-    // Get the updated customer details
     var customerId = document.getElementById("txtCustomerId").value;
     var customerName = document.getElementById("txtCustomerName").value;
     var address = document.getElementById("txtAddress").value;
     var phoneNumber = document.getElementById("txtPhoneNumber").value;
 
-    // Find the customer to update
     for (var i = 0; i < customerDetails.length; i++) {
       if (customerDetails[i].customerId == customerId) {
-        // Update the customer details
+        alert("Are you sure you want to update customer information?");
         customerDetails[i].customerName = customerName;
         customerDetails[i].address = address;
         customerDetails[i].phoneNumber = phoneNumber;
 
-        // Update the table row for the customer
         var row = document.getElementById("customerTable").rows[i + 1];
         row.cells[1].textContent = customerName;
         row.cells[2].textContent = address;
         row.cells[3].textContent = phoneNumber;
 
-        // Optionally, add a success message
         console.log("Customer details updated successfully");
         break;
       }
@@ -97,6 +93,7 @@ document
     var customerIdToDelete = document.getElementById("txtCustomerId").value;
     for (var i = 0; i < customerDetails.length; i++) {
       if (customerDetails[i].customerId == customerIdToDelete) {
+        alert("Are you sure you want to delete customer information?");
         customerDetails.splice(i, 1);
         break;
       }
@@ -111,164 +108,82 @@ document
     }
   });
 
-//     // Loop through customerDetails array and find the index of the customer to be updated
-//     var indexToUpdate = -1;
-//     for (var i = 0; i < customerDetails.length; i++) {
-//       if (customerDetails[i].customerId === customerId) {
-//         indexToUpdate = i;
-//         break;
-//       }
-//     }
+//clear all textfield by click button
+document
+  .getElementById("btnCustomerClearAll")
+  .addEventListener("click", function () {
+    document.getElementById("txtCustomerId").value = "";
+    document.getElementById("txtCustomerName").value = "";
+    document.getElementById("txtAddress").value = "";
+    document.getElementById("txtPhoneNumber").value = "";
+  });
 
-//     // Update the customer's properties in the customerDetails array
-//     if (indexToUpdate >= 0) {
-//       customerDetails[indexToUpdate].customerName =
-//         updatedCustomer.customerName;
-//       customerDetails[indexToUpdate].address = updatedCustomer.address;
-//       customerDetails[indexToUpdate].phoneNumber = updatedCustomer.phoneNumber;
-//     }
+//input field data validation
+document.getElementById("txtCustomerId").addEventListener("input", function () {
+  validateCustomerId();
+});
 
-//     // Update the selected row in the customer table
-//     var selectedRow = document.querySelector("tr.selected");
-//     if (selectedRow) {
-//       selectedRow.cells[1].textContent = updatedCustomer.customerName;
-//       selectedRow.cells[2].textContent = updatedCustomer.address;
-//       selectedRow.cells[3].textContent = updatedCustomer.phoneNumber;
-//     }
-//     console.log(customerDetails);
-//   });
+//validate customer id field
+function validateCustomerId() {
+  var customerId = document.getElementById("txtCustomerId").value;
+  var customerIdRegex = /^C\d{2}-\d{3}$/;
 
-// // customer save
+  if (customerIdRegex.test(customerId)) {
+    document.getElementById("txtCustomerId").style.border = "solid 1px green";
+    document.getElementById("txtCustomerName").focus();
+    document.getElementById("customerIdvalidationNote").style.display = "none";
+  } else {
+    document.getElementById("txtCustomerId").style.border = "solid 1px red";
+    document.getElementById("customerIdvalidationNote").style.display = "block";
+  }
+}
 
-// /*  document.getElementById("btnCustomerUpdate").addEventListener("click", function () {
-//     var selectedRow = document.querySelector("#customerTable tr.selected");
-//     console.log(selectedRow)
-//     if (selectedRow) {
-//       var col1 = selectedRow.cells[0];
-//       var col2 = selectedRow.cells[1];
-//       var col3 = selectedRow.cells[2];
-//       var col4 = selectedRow.cells[3];
+//validate customer name field
+document
+  .getElementById("txtCustomerName")
+  .addEventListener("input", function () {
+    validateCustomerName();
+  });
 
-//       col1.textContent = txtCustomerId.value;
-//       col2.textContent = txtCustomerName.value;
-//       col3.textContent = txtAddress.value;
-//       col4.textContent = txtPhoneNumber.value;
+function validateCustomerName() {
+  var customerName = document.getElementById("txtCustomerName").value;
+  var customerNameRegex = /^[A-Za-z\s]+$/;
 
-//       selectedRow.classList.remove("selected");
-//     }
-//   });*/
+  if (customerName.length >= 10 && customerNameRegex.test(customerName)) {
+    document.getElementById("txtCustomerName").style.border = "solid 1px green";
+    document.getElementById("txtAddress").focus();
+    document.getElementById("customerNamevalidationNote").style.display =
+      "none";
+  } else {
+    document.getElementById("txtCustomerName").style.border = "solid 1px red";
+    document.getElementById("customerNamevalidationNote").style.display =
+      "block";
+  }
+}
 
-// $("#btnCustomerUpdate").click(function () {
-//   alert("need update");
-//   cusID = $("#txtProductId").val();
-//   cusName = $("#txtProductDescription").val();
-//   cusAddress = $("#txtUnitPrice").val();
-//   cusContact = $("#txtQTY").val();
+document.getElementById("txtAddress").addEventListener("input", function () {
+  validateAddress();
+});
 
-//   console.log(cusID, cusName, cusAddress, cusContact);
+function validateAddress() {
+  var address = document.getElementById("txtAddress").value;
+  var addressRegex = /^[A-Za-z0-9]+$/;
 
-//   // Find the customer to update in the existing customers array (assuming it's named 'allCustomers')
-//   let customerToUpdate = allCustomers.find(function (customer) {
-//     return customer.ID === cusID;
-//   });
+  if (addressRegex.test(address)) {
+    document.getElementById("txtAddress").style.border = "solid 1px green";
+    document.getElementById("txtPhoneNumber").focus();
+    document.getElementById("customerAddressvalidationNote").style.display =
+      "none";
+  } else {
+    document.getElementById("txtAddress").style.border = "solid 1px red";
+    document.getElementById("customerAddressvalidationNote").style.display =
+      "block";
+  }
+}
 
-//   if (customerToUpdate) {
-//     // Update the customer details
-//     customerToUpdate.name = cusName;
-//     customerToUpdate.address = cusAddress;
-//     customerToUpdate.content = cusContact;
-
-//     console.log(allCustomers);
-
-//     // Find the corresponding table row and update its content
-//     let rowToUpdate = $("#cusTableBody")
-//       .find("td:contains(" + cusID + ")")
-//       .closest("tr");
-//     rowToUpdate.html(
-//       "<td>" +
-//         cusID +
-//         "</td><td>" +
-//         cusName +
-//         "</td><td>" +
-//         cusAddress +
-//         "</td><td>" +
-//         cusContact +
-//         "</td>"
-//     );
-
-//     console.log("Customer updated successfully.");
-//   } else {
-//     console.log("Customer with ID " + cusID + " not found.");
-//   }
+// $("#txtPhoneNumber").on("input", function () {
+//   validatePhoneNumber();
 // });
-
-// $("#btnCustomerUpdate").on("click", function () {
-//   // Get the selected row
-//   var selectedRow = $("#customerTable tr.selected");
-
-//   // If a row is selected, update its data
-//   if (selectedRow.length > 0) {
-//     selectedRow.children("td:eq(0)").text($("#txtCustomerId").val());
-//     selectedRow.children("td:eq(1)").text($("#txtCustomerName").val());
-//     selectedRow.children("td:eq(2)").text($("#txtAddress").val());
-//     selectedRow.children("td:eq(3)").text($("#txtPhoneNumber").val());
-
-//     // Clear the selection
-//     selectedRow.removeClass("selected");
-//   }
-// });
-
-// $("#txtCustomerId").on("input", function () {
-//   validateCustomerId();
-// });
-
-// function validateCustomerId() {
-//   var customerId = $("#txtCustomerId").val();
-//   var customerIdRegex = /^C\d{2}-\d{3}$/;
-
-//   if (customerIdRegex.test(customerId)) {
-//     $("#txtCustomerId").css("border", "solid 1px green");
-//     $("#txtCustomerName").focus();
-//   } else {
-//     $("#txtCustomerId").css("border", "solid 1px red");
-//   }
-// }
-
-// $("#txtCustomerName").on("input", function () {
-//   validateCustomerName();
-// });
-
-// function validateCustomerName() {
-//   var customerName = $("#txtCustomerName").val();
-//   var customerNameRegex = /^[A-Za-z\s]+$/;
-
-//   if (customerName.length === 10 && customerNameRegex.test(customerName)) {
-//     $("#txtCustomerName").css("border", "solid 1px green");
-//     $("#txtAddress").focus();
-//   } else {
-//     $("#txtCustomerName").css("border", "solid 1px red");
-//   }
-// }
-
-// /*  $("#txtAddress").on("input", function() {
-//     validateAddress();
-//   });
-
-//   function validateAddress() {
-//     var address = $("#txtAddress").val();
-//     var addressRegex = /^[A-Za-z0-9]+$/;
-
-//     if (addressRegex.test(address)) {
-//       $("#txtAddress").css("border","solid 1px green");
-//       $("#txtPhoneNumber").focus();
-//     } else {
-//       $("#txtAddress").css("border","solid 1px red");
-//     }
-//   }
-
-//   $("#txtPhoneNumber").on("input", function() {
-//     validatePhoneNumber();
-//   });*/
 
 // function validatePhoneNumber() {
 //   var phoneNumber = $("#txtPhoneNumber").val();
