aa08dcc2b3f551c6e1db96018b17c68e1c91909a
sehan22 <sehanranaweera2002@gmail.com>
2023-06-26 00:05:36 +0530

    web POS updated

---
+++ InClass Assignments/JS/InMemory-POS/db/db.js
@@ -0,0 +1,12 @@
+var customerdb=[
+    {id: "C00-001", name: "Saman Kumara", address: "Galle", contact: "0717741744"},
+    {id: "C00-002", name: "Sehan Ranaweera", address: "Matara", contact: "0712400167"},
+    {id: "C00-003", name: "Sunil Perera", address: "Colombo", contact: "0702390668"},
+    {id: "C00-004", name: "Nihal Ranjith", address: "Weligama", contact: "0778596789"}
+];
+var productdb=[
+    {id:"P00-001",description:"hair gel",unitPrice: 750.00,qtyOnHand: 20},
+    {id:"P00-002",description:"soap",unitPrice: 215.00,qtyOnHand: 135},
+    {id:"P00-003",description:"Rice",unitPrice: 995.00,qtyOnHand: 18},
+    {id:"P00-003",description:"Toy",unitPrice: 1350.00,qtyOnHand: 6}
+];
